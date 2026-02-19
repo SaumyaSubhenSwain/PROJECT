97624ff2df12c2a72bdee8888b0fc65a578d53bb
saumya <swainsaumya413@gmail.com>
2026-02-19 14:32:08 +0530

    xpath

--- Locators/xpath_css/xpath.py
+++ Locators/xpath_css/xpath.py
@@ -13,6 +13,20 @@ with sync_playwright() as p:
     username = page.locator("//input[@name='username']")
     username.fill("My name is saumya")
 
+    #2. using tagname (//tagname[text()='value'])
+    login = page.locator("//button[text()='Login']")
+    login.click()
+    print(login.text_content())
+
+    #3. using contains :- mostly doing the partial match (//tagname[conatins(@attribute/text(),"value")])
+    click = page.locator("//button[contains(text(),'OTP')]")
+    click.click()
+    print(click.text_content())
+
+    #4 using normalize-space :- remove the unauthorized space from the text
+    # //button[normalize-space() = .'login']
+
+    #starts with :- matching some first characters
 
     time.sleep(5)
 
